a69446a4a057780bac3292192edaf0528e2540ba
Christian Hoene <christian.hoene@symonics.com>
2026-03-07 22:58:45 +0100

    generating an empty analog design

--- docs/info.md
+++ docs/info.md
@@ -9,12 +9,12 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-Explain how your project works
+It shall generate a clock of 24 Mhz.
 
 ## How to test
 
-Explain how to use your project
+Start the design and watch the output.
 
 ## External hardware
 
-List external hardware used in your project (e.g. PMOD, LED display, etc), if any
+None.
---
+++ magic_init_project.tcl
@@ -0,0 +1,53 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2024-2026 Tiny Tapeout LTD
+# Author: Uri Shaked
+# Description: This script initializes a new Magic project for an analog design on Tiny Tapeout.
+
+# Important: before running this script, download the the .def file from
+# https://raw.githubusercontent.com/TinyTapeout/tt-support-tools/main/tech/ihp-sg13g2/def/analog/tt_analog_1x2.def
+
+# Change the settings below to match your design:
+# ------------------------------------------------
+set TOP_LEVEL_CELL     tt_um_hoene_firsttry
+set TEMPLATE_FILE      tt_analog_1x2.def
+set POWER_STRIPE_WIDTH 2.4um                 ;# The minimum width is 2.1um
+
+# Power stripes: NET name, x position. You can add additional power stripes for each net, as needed.
+# Min spacing: 1.64um.
+set POWER_STRIPES {
+    VDPWR 1um
+    VGND  6um
+}
+
+# Read in the pin positions
+# -------------------------
+def read $TEMPLATE_FILE
+cellname rename tt_um_template $TOP_LEVEL_CELL
+
+# Draw the power stripes
+# --------------------------------
+proc draw_power_stripe {name x} {
+    global POWER_STRIPE_WIDTH
+    box $x 5um $x 308um
+    box width $POWER_STRIPE_WIDTH
+    paint met6
+    label $name FreeSans 0.25u -met6
+    port make
+    port use [expr {$name eq "VGND" ? "ground" : "power"}]
+    port class bidirectional
+    port connections n s e w
+}
+
+# You can extra power stripes, as you need.
+foreach {name x} $POWER_STRIPES {
+    puts "Drawing power stripe $name at $x"
+    draw_power_stripe $name $x
+}
+
+# Save the layout and export GDS/LEF
+# ----------------------------------
+save ${TOP_LEVEL_CELL}.mag
+file mkdir gds
+gds write gds/${TOP_LEVEL_CELL}.gds
+file mkdir lef
+lef write lef/${TOP_LEVEL_CELL}.lef -hide -pinonly
